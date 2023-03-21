6982a266d8bb4c15563607a37be3293ad7d36477
joshuaraow <126080827+joshuaraow@users.noreply.github.com>
2023-03-21 16:57:31 +0100

    Update 1

---
+++ JoshuaRaow21.03.js
@@ -0,0 +1,31 @@
+//Exo 1
+let age = 25;
+
+//Exo2
+let nom = "Jean";
+
+//Exo3
+let somme = 12 + 12;
+
+//Exo4
+let chaine = chaine.toUpperCase();
+
+//Exo5
+let tableau = [1,2,3,4,5];
+let longueur = tableau.length;
+
+//Exo6
+let objet = {nom: 'Joshua', age:24};
+console.log(objet['nom']);
+
+//Exo7
+let nombre = 12.98;
+document.write(nombre.toFixed(2));
+
+//Exo8
+let date = new Date ('March 21, 23 16:48:35');
+console.log(date.getMonth());
+
+//Exo9
+let tableau2 = ["tableau","de","chaines"];
+console.log(join());
